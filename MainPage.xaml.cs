f6721c619399d91f8adc8d64bf2d0ef612877b27
S4pphiree <s4pph1r3sam@gmail.com>
2022-11-16 19:49:31 +0100

    Program's Finished!

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Maui.ApplicationModel.Communication;
+﻿using AS2223_4G_INF_IngrassiaSamuele_Figure;
+using Microsoft.Maui.ApplicationModel.Communication;
 
 namespace FigureMAUI;
 
@@ -8,10 +9,10 @@ public partial class MainPage : ContentPage
     {
         InitializeComponent();
     }
-    void OnPickerSelectedIndexChanged(object sender, EventArgs e)
-    {
-        
-    }
+
+    int n = 0;
+    string chosenShape;
+    string[] lastCalculationsList = new string[10];
 
     private void ShowGeometry_Clicked(object sender, EventArgs e)
     {
@@ -23,8 +24,13 @@ public partial class MainPage : ContentPage
         lblRadius.IsVisible = false;
         txtHeight.Text = "";
         txtBase.Text = "";
+        
+        lblCalculate.IsVisible = true;
+        btnCalculate.IsVisible = true;
+        
+        chosenShape = cmbShapes.SelectedItem.ToString();
 
-        switch (cmbShapes.SelectedItem)
+        switch (chosenShape)
         {
             case "Quadrato":
                 lblBase.IsVisible = true;
@@ -42,11 +48,42 @@ public partial class MainPage : ContentPage
                 txtBase.IsVisible = true;
                 break;
         }
+
+
+
+        lblCalculate.Text = $"Calcolerò area e perimetro di un {chosenShape}";
     }
 
     private void Calculate_Clicked(object sender, EventArgs e)
     {
-        
+        string shapeInfo = "";
+
+        switch (chosenShape)
+        {
+            case "Quadrato":
+                Square sq = new Square(Double.Parse(txtBase.Text));
+                shapeInfo = $"Area: {sq.Area}";
+                break;
+            case "Rettangolo":
+                Rectangle rect = new Rectangle(Double.Parse(txtBase.Text), Double.Parse(txtHeight.Text));
+                shapeInfo = $"Area: {rect.Area} | Perimetro: {rect.Perimeter}";
+                break;
+            case "Cerchio":
+                Circle circ = new Circle(Double.Parse(txtBase.Text));
+                shapeInfo = $"Area: {circ.Area} | Diametro: {circ.Perimeter}";
+                break;
+        }
+
+        if (n == 10)
+            n = 0;
+
+        lastCalculationsList[n] = $"{chosenShape} | {shapeInfo}";
+        n++;
+
+        Array.Resize<string>(ref lastCalculationsList, n + 1);
+
+        lstLastCalculations.ItemsSource = lastCalculationsList;
+
     }
 }
 
